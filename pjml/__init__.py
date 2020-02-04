ec13d81cb127e9362f0b0e4edf0792bea2a7f3c2
davips <dpsabc@gmail.com>
2020-02-04 18:03:03 -0300

    added option to import * all pjml modules for better use of notebooks

--- pjml/__init__.py
+++ pjml/__init__.py
@@ -1,1 +1,9 @@
 from ._version import __version__  # noqa: ignore
+from pathlib import Path
+
+# Include all classes when 'from pjml import *' is called.
+fs = [f for f in Path('pjml').rglob('*.py') if not f.name.startswith('_')]
+
+for f in [str(f).replace('/', '.')[:-3] for f in fs]:
+    statement = f'from {f} import *'
+    exec(statement)
