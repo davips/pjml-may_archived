32d7f8182369abcb6185f49555d739d4d8e91b2d
davips <dpsabc@gmail.com>
2020-02-05 21:51:05 -0300

    pipeline macro were missing

--- pjml/useful.py
+++ pjml/useful.py
@@ -5,12 +5,12 @@ paths = list(Path('pjml/tool').rglob('*.py')) + \
 fs = [str(f) for f in paths
       if not f.name.startswith('_')
       if not f.name.startswith('reduce')
-      if '/abc/' not in str(f)]
+      if '/abc/' not in str(f)] + ['pjml/pipeline.py', 'pjml/macro.py']
 
 for f in [f.replace('/', '.')[:-3] for f in fs]:
     statement = f'from {f} import *'
     print(statement)
     exec(statement)
 
-things = 'All tools and operators imported (and corresponding pollutant ' \
-         'namespaces as well)!'
+print('All tools and operators imported'
+      ' (and corresponding pollutant namespaces as well)!')
