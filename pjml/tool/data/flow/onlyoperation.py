358a020648b87a6ada69c16495c4b91b65d677ec
e.alcobaca <e.alcobaca@gmail.com>
2020-03-04 19:34:17 -0300

    Update to deal with CS

--- pjml/tool/data/flow/onlyoperation.py
+++ pjml/tool/data/flow/onlyoperation.py
@@ -1,12 +1,20 @@
 # def onlyapply():
+from pjml.config.description.cs.containercs import ContainerCS
 from pjml.tool.abc.nonconfigurablecontainer1 import NonConfigurableContainer1
 from pjml.tool.abc.singleton import NoModel
+from pjml.tool.abc.transformer import Transformer
 
 
 class OnlyApply(NonConfigurableContainer1):
     """Does nothing during 'apply'."""
 
-    # TODO: implement __new__ to generate a CS
+    def __new__(cls, *args, transformers=None):
+        """Shortcut to create a ConfigSpace."""
+        if transformers is None:
+            transformers = args
+        if all([isinstance(t, Transformer) for t in transformers]):
+            return object.__new__(cls)
+        return ContainerCS(OnlyApply.name, OnlyApply.path, transformers)
 
     def _apply_impl(self, data):
         self.model = NoModel
@@ -19,7 +27,13 @@ class OnlyApply(NonConfigurableContainer1):
 class OnlyUse(NonConfigurableContainer1):
     """Does nothing during 'apply'."""
 
-    # TODO: implement __new__ to generate a CS
+    def __new__(cls, *args, transformers=None):
+        """Shortcut to create a ConfigSpace."""
+        if transformers is None:
+            transformers = args
+        if all([isinstance(t, Transformer) for t in transformers]):
+            return object.__new__(cls)
+        return ContainerCS(OnlyUse.name, OnlyUse.path, transformers)
 
     def _apply_impl(self, data):
         self.model = NoModel
