de25db2b38f53041cb51676162df00055e6c4cf7
e.alcobaca <e.alcobaca@gmail.com>
2020-03-04 19:26:41 -0300

    update full function to put final data from 'apply' in the 'use' step

--- pjml/config/operator/reduction/full.py
+++ pjml/config/operator/reduction/full.py
@@ -6,6 +6,9 @@ from pjml.config.description.cs.configlist import ConfigList
 
 # nonTODO: PEP 8 requires lowercase in function names; so is that ok to use a
 #  class instead? Or should we change all operators to function/lowercase?
+from pjml.tool.data.manipulation.keep import Keep
+
+
 def full(cs, data=NoData, n=1, field='S'):
     """Exhaustive search to maximize value at 'field'.
 
@@ -16,9 +19,11 @@ def full(cs, data=NoData, n=1, field='S'):
 
     results = []
     for pipe in cs:
-        pipe.apply(data)
-        res = pipe.use(data).field(field, 'full search').item(0)
-        results.append((pipe, -res))
+        p = pipe
+        result_data = p.apply()
+        print('---------------', result_data.matrices.keys())
+        res = p.use(result_data).field(field, 'full search').item(0)
+        results.append((p, -res))
 
     pipes = [x[0] for x in sorted(results, key=itemgetter(1))[:n]]
     return ConfigList(transformers=pipes)
