c7009793f64f977f11ea4dfd8a4bdcd1390e97da
e.alcobaca <e.alcobaca@gmail.com>
2020-03-04 19:46:40 -0300

    debug flags removed of example

--- examples/automl_multi_obj.py
+++ examples/automl_multi_obj.py
@@ -1,27 +1,18 @@
-import numpy
-
-from cururu.persistence import DuplicateEntryException, FailedEntryException
-from pjdata.data_creation import read_arff
 from pjml.config.operator.many import shuffle, select
 from pjml.config.operator.reduction.full import full
 from pjml.config.operator.reduction.rnd import rnd
-from pjml.macro import evaluator
 from pjml.pipeline import Pipeline
 from pjml.tool.chain import Chain
 from pjml.tool.collection.expand.partition import Partition
 from pjml.tool.collection.reduce.summ import Summ
 from pjml.tool.collection.transform.map import Map
-from pjml.tool.data.communication.cache import Cache
 from pjml.tool.data.communication.report import Report
 from pjml.tool.data.evaluation.calc import Calc
 from pjml.tool.data.evaluation.mconcat import MConcat
 from pjml.tool.data.evaluation.metric import Metric
-from pjml.tool.data.evaluation.split import Split
 from pjml.tool.data.flow.applyusing import ApplyUsing
 from pjml.tool.data.flow.file import File
 from pjml.tool.data.flow.onlyoperation import OnlyApply, OnlyUse
-from pjml.tool.data.flow.source import Source
-from pjml.tool.data.flow.sink import Sink
 from pjml.tool.data.manipulation.copy import Copy
 from pjml.tool.data.manipulation.keep import Keep
 from pjml.tool.data.modeling.supervised.classifier.dt import DT
@@ -54,11 +45,11 @@ expr = Pipeline(
         OnlyApply(Copy(from_field="S", to_field="B")),
         OnlyUse(MConcat(input_field1="B", input_field2="S",
                         output_field="S", direction='vertical')),
-        Report(' S --> \n$S'),
+        # Report(' S --> \n$S'),
         Calc(function=['flatten']),
-        Report('flatten S --> \n$S'),
+        # Report('flatten S --> \n$S'),
         Calc(function=['mean']),
-        Report('mean S --> \n$S'),
+        Report('mean S --> $S'),
         fields=["X", "Y"]
     )
 )
@@ -66,10 +57,8 @@ expr = Pipeline(
 # diversidade,
 # Lambda(function='$R[0][0] * $R[0][1]', field='r')
 
-# print(expr)
 print('sample .................')
 pipe = full(rnd(expr, n=5), field='S', n=1).sample()
-# print(pipe)
 
 print('apply .................')
 data = File("iris.arff").apply()
