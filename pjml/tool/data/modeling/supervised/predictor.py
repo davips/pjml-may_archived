d043bc72a3a05e8835d32bd525363bf4e6482efa
e.alcobaca <e.alcobaca@gmail.com>
2020-03-06 22:03:55 -0300

    Update to new functional Pajé version

--- pjml/tool/data/modeling/supervised/predictor.py
+++ pjml/tool/data/modeling/supervised/predictor.py
@@ -2,6 +2,7 @@ from abc import ABC
 
 from pjdata.step.use import Use
 from pjml.tool.abc.mixin.exceptionhandler import BadComponent
+from pjml.tool.abc.model import Model
 from pjml.tool.abc.transformer import Transformer
 
 
@@ -10,14 +11,25 @@ class Predictor(Transformer, ABC):
     Base class for classifiers, regressors, ... that implement fit/predict.
     """
 
-    def _apply_impl(self, data):
-        self.algorithm.fit(*data.Xy)
-        self.model = self.algorithm
-        return None
+    def __init__(self, config, algorithm_factory, sklearn_config,
+                 deterministic=False):
+        super().__init__(config, deterministic)
+        self.algorithm_factory = algorithm_factory
+        self.sklearn_config = sklearn_config
 
-    def _use_impl(self, data):
-        return data.updated(self.transformations(),
-                            z=self.algorithm.predict(data.X))
+    def _apply_impl(self, data_apply):
+        sklearn_model = self.algorithm_factory(**self.sklearn_config)
+        sklearn_model.fit(*data_apply.Xy)
+
+        class PredictorModel(Model):
+            def _data_impl(self):
+                return None
+
+            def _use_impl(self, data_use):
+                return data_use.updated(self.transformations(),
+                                        z=sklearn_model.predict(data_use.X))
+
+        return PredictorModel()
 
     def transformations(self, step=None, training_data=None):
         if step is None:
