087719f40475d2f0f7a7beebdc7af3c41314666c
e.alcobaca <e.alcobaca@gmail.com>
2020-03-06 22:06:01 -0300

    Update - NoDataHandler

--- pjml/tool/abc/container.py
+++ pjml/tool/abc/container.py
@@ -2,11 +2,11 @@ from abc import ABC
 from functools import lru_cache
 
 from pjdata.aux.decorator import classproperty
-from pjml.tool.abc.transformer_nodata import Transformer_NoData
+from pjml.tool.abc.nodatahandler import NoDataHandler
 from pjml.util import flatten
 
 
-class Container(Transformer_NoData, ABC):
+class Container(NoDataHandler, ABC):
     """A container modifies 'transformer(s)'."""
 
     def __init__(self, transformers):
