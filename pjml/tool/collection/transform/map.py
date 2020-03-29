91445e6d1ed42d87320cfb896d7d3e5f9393f5d2
ealcobaca <e.alcobaca@gmail.com>
2020-03-29 17:26:45 -0300

    functional update

--- pjml/tool/collection/transform/map.py
+++ pjml/tool/collection/transform/map.py
@@ -3,6 +3,7 @@ from pjml.tool.abc.transformer import Transformer
 from pjml.tool.abc.nonconfigurablecontainer1 import NonConfigurableContainer1
 from pjdata.infinitecollection import InfiniteCollection
 
+from pjml.tool.model import ContainerModel
 
 
 class Map(NonConfigurableContainer1):
@@ -19,23 +20,26 @@ class Map(NonConfigurableContainer1):
     def _apply_impl(self, collection):
         if isinstance(collection, InfiniteCollection):
             raise Exception('Collection should be finite for Map!')
-        self.model = []
+        models = []
         datas = []
         for data in collection:
             transformer = self.transformer.clone()
-            output_data = transformer.apply(data, self._exit_on_error)
-            datas.append(output_data)
-            self.model.append(transformer)
-        return collection.updated(self.transformations(), datas=datas)
+            model = transformer.apply(data, self._exit_on_error)
+            datas.append(model.data)
+            models.append(transformer) # dar append no output_data?
+    
+        def use_impl(collection):
+            size = len(models)
+            if size != collection.size:
+                raise Exception('Collections passed to apply and use should have '
+                                f'the same size a- {size} != u- {collection.size}')
+            datas = []
+            for model in models:
+                data = model.use(next(collection), self._exit_on_error)
+                datas.append(data)
+            return collection.updated(self.transformations(step='u'), datas=datas)
+        # TODO: which containers should pass self._exit_on_error to transformer?
 
-    def _use_impl(self, collection):
-        size = len(self.model)
-        if size != collection.size:
-            raise Exception('Collections passed to apply and use should have '
-                            f'the same size a- {size} != u- {collection.size}')
-        datas = []
-        for transformer in self.model:
-            data = transformer.use(next(collection), self._exit_on_error)
-            datas.append(data)
-        return collection.updated(self.transformations(), datas=datas)
-    # TODO: which containers should pass self._exit_on_error to transformer?
+        applied = collection.updated(self.transformations(step='a'), datas=datas)
+
+        return ContainerModel(models, applied, self, use_impl)
