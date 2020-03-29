9d19c48bdf4321409dc5316476d39c74dec955e4
ealcobaca <e.alcobaca@gmail.com>
2020-03-29 17:24:29 -0300

    AutoML example

--- examples/func_toy.py
+++ examples/func_toy.py
@@ -1,12 +1,12 @@
 from pjml.pipeline import Pipeline
+from pjml.tool.collection.expand.partition import Partition
+from pjml.tool.collection.reduce.summ import Summ
+from pjml.tool.collection.transform.map import Map
 from pjml.tool.data.communication.report import Report
 from pjml.tool.data.evaluation.metric import Metric
-from pjml.tool.data.evaluation.split import Split
 from pjml.tool.data.flow.applyusing import ApplyUsing
 from pjml.tool.data.flow.file import File
-from pjml.tool.data.manipulation.keep import Keep
-from pjml.tool.data.modeling.supervised.classifier.dt import DT
-from pjml.tool.data.modeling.supervised.classifier.nb import NB
+from pjml.tool.data.modeling.supervised.classifier.rf import RF
 from pjml.tool.data.modeling.supervised.classifier.svmc import SVMC
 
 from pjml.tool.data.processing.feature.binarize import Binarize
@@ -14,11 +14,17 @@ from pjml.tool.data.processing.feature.binarize import Binarize
 # pipe = Pipeline(File("iris.arff"), ApplyUsing(NB()), Metric(), Report())
 from pjml.tool.data.processing.instance.sampler.under.random import UnderS
 
-pipe = Pipeline(File("abalone3.arff"), Binarize(),
-                Split(),
-                UnderS(sampling_strategy='not minority'),
-                ApplyUsing(NB()), Metric(), Report(),
-                ApplyUsing(SVMC()), Metric(), Report())
+pipe = Pipeline(
+    File("abalone3.arff"), Binarize(),
+    Partition(),
+    # Map(
+    #     UnderS(sampling_strategy='not minority'),
+    #     RF(),
+    #     Metric()
+    # ),
+    # Summ(function='mean_std'),
+    # Report('mean S --> $S')
+)
 
 model = pipe.apply()
 # print(1111111111111111, model)
