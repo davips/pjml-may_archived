ee3dfc7ef7986e29636d81c10837a625ba7acee5
ealcobaca <e.alcobaca@gmail.com>
2020-03-29 11:25:57 -0300

    EnforceApply added

--- pjml/tool/data/modeling/supervised/sklpredictor.py
+++ pjml/tool/data/modeling/supervised/sklpredictor.py
@@ -1,12 +1,14 @@
 from abc import ABC
 
 from pjdata.step.use import Use
+
+from pjml.tool.abc.mixin.enforceapply import EnforceApply
 from pjml.tool.abc.mixin.exceptionhandler import BadComponent
 from pjml.tool.data.sklalgorithm import SKLAlgorithm
 from pjml.tool.model import Model
 
 
-class SKLPredictor(SKLAlgorithm, ABC):
+class SKLPredictor(SKLAlgorithm, EnforceApply, ABC):
     """
     Base class for classifiers, regressors, ... that implement fit/predict.
     """
@@ -23,9 +25,7 @@ class SKLPredictor(SKLAlgorithm, ABC):
 
         return Model(None, self, use_impl)
 
-    def transformations(self, step=None):
-        if step is None:
-            step = self._current_step
+    def transformations(self, step):
         if step == 'a':
             return []
         elif step == 'u':
