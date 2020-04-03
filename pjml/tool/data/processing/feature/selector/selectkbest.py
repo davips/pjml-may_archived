5b2bacaa1d33dee840b2857776f5e2be90cf3b20
davips <dpsabc@gmail.com>
2020-04-02 21:28:07 -0300

    SelectBest seems ready

--- pjml/tool/data/processing/feature/selector/selectkbest.py
+++ pjml/tool/data/processing/feature/selector/selectkbest.py
@@ -1,53 +1,51 @@
-from functools import partial
-
-from sklearn.feature_selection import f_classif, mutual_info_classif, SelectPercentile, SelectFpr, SelectFdr, \
-    SelectFwe, GenericUnivariateSelect, SelectKBest
+import numpy as np
+from numpy.random import uniform
+from sklearn.feature_selection import f_classif, mutual_info_classif, \
+    SelectPercentile, SelectFpr, SelectFdr, \
+    SelectFwe, GenericUnivariateSelect, SelectKBest, chi2
 
 from pjdata.step.use import Use
 from pjml.config.description.cs.transformercs import TransformerCS
 from pjml.config.description.distributions import choice
 from pjml.config.description.node import Node
-from pjml.config.description.parameter import CatP
+from pjml.config.description.parameter import CatP, RealP
 from pjml.tool.abc.mixin.exceptionhandler import BadComponent
 from pjml.tool.data.algorithm import Algorithm
 from pjml.tool.model import Model
 
 
-class SelectKB(Algorithm):
+class SelectBest(Algorithm):
     SCORE_FUNCTIONS = {
+        "chi2": chi2,
         "f_classif": f_classif,
-        "mutual_info_classif": mutual_info_classif,
-        "SelectPercentile": SelectPercentile,
-        "SelectFpr": SelectFpr,
-        "SelectFdr": SelectFdr,
-        "SelectFwe": SelectFwe,
-        "GenericUnivariateSelect": GenericUnivariateSelect
+        "mutual_info_classif": mutual_info_classif
     }
 
-    def __init__(self, **kwargs):
-        internal_kwargs = kwargs.copy()
-        if "score_func" in kwargs:
-            internal_kwargs["score_func"] = self.SCORE_FUNCTIONS[kwargs["score_func"]]
+    def __init__(self, score_func, k_perc):
+        sklearn_score = self.SCORE_FUNCTIONS[score_func]
 
-        algorithm_factory = partial(SelectKBest, **internal_kwargs)
-        super().__init__(kwargs, algorithm_factory)
+        def algorithm_factory(nfeatures):
+            return SelectKBest(
+                score_func=sklearn_score,
+                k=int(np.ceil(nfeatures * k_perc))
+            )
+
+        config = {'score_func': score_func, 'k_perc': k_perc}
+        super().__init__(config, algorithm_factory)
 
     @classmethod
     def _cs_impl(cls):
         params = {
             'score_func': CatP(
                 choice,
-                items=[
-                    "chi2", "f_classif", "mutual_info_classif",
-                    "SelectPercentile", "SelectFpr", "SelectFdr",
-                    "SelectFwe", "GenericUnivariateSelect"
-                ]
+                items=["chi2", "f_classif", "mutual_info_classif"]
             ),
+            'k_perc': RealP(uniform, low=0.0, high=1.0)
         }
-        return TransformerCS(Node(params=params))
+        return TransformerCS(nodes=[Node(params=params)])
 
     def _apply_impl(self, data):
-        sklearn_model = self.algorithm_factory()
+        sklearn_model = self.algorithm_factory(data.X.shape[1])
         X_new = sklearn_model.fit_transform(*data.Xy)
         applied = data.updated(self.transformations('a'), X=X_new)
         return Model(self, applied, sklearn_model)
