912ee3aa98404b5de75981853a1fd2ddf6e72ee2
davips <dpsabc@gmail.com>
2020-04-17 00:37:50 -0300

    MFE

--- pjml/tool/meta/mfe.py
+++ pjml/tool/meta/mfe.py
@@ -2,8 +2,8 @@ import numpy as np
 import pymfe.mfe
 
 from pjml.config.description.cs.emptycs import EmptyCS
-from pjml.tool.abc.singleton import NoAlgorithm
 from pjml.tool.abc.transformer import Transformer
+from pjml.tool.model.model import Model
 
 
 class MFE(Transformer):
@@ -15,18 +15,18 @@ class MFE(Transformer):
     Applying a normalization after this transformer is recommended."""
 
     def __init__(self):
-        super().__init__({}, NoAlgorithm, deterministic=True)
-        self.model = pymfe.mfe.MFE()
+        super().__init__({}, deterministic=True)
 
     @classmethod
     def _cs_impl(cls):
         return EmptyCS()
 
     def _apply_impl(self, data):
-        self.model.fit(*data.Xy)
-        ft = self.model.extract()
-        return data.updated(self.transformations(),
-                            M=np.array([ft[1]]), Md=ft[0])
+        applied = self._use_impl(data, step='a')
+        return Model(self, data, applied)
 
-    def _use_impl(self, data, **kwargs):
-        return self._apply_impl(data)
+    def _use_impl(self, data, step='u'):
+        pymfe.mfe.MFE().fit(*data.Xy)
+        ft = pymfe.mfe.MFE().extract()
+        return data.updated(self.transformations(step),
+                            M=np.array([ft[1]]), Md=ft[0])
